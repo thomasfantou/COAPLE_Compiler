fd3a9abc5554584eacc119468e8aa8d84a5d470b
Thomas Fantou <tbfantou@gmail.com>
2013-05-22 15:28:01 +0100

    first commit

---
+++ src/co/uk/brookes/main.java
@@ -0,0 +1,12 @@
+package co.uk.brookes;
+
+/**
+ * Author : Fantou Thomas
+ * Date: 5/22/13
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+}
